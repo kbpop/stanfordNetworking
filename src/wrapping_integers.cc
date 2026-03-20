983044af94326447036602a25c481f742d0a0bfd
Ubuntu <ubuntu@ip-172-31-37-182.us-east-2.compute.internal>
2026-03-20 02:26:04 +0000

    Changed the wrapper

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -10,38 +10,30 @@ Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 {
   // Your code here.
   debug( "unimplemented wrap( {}, {} ) called", n, zero_point.raw_value_ );
-  uint32_t zero = 0;
+  uint32_t zero = ~0;
   uint64_t offset = n + zero_point.raw_value_;
-  while(offset > ~zero){
-    offset -= ~zero;
-    offset--;
-  }
-  
-  return Wrap32 { (uint32_t)offset };
+  return Wrap32 { (uint32_t)(offset % zero) };
 }
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
   // Your code here.
   debug( "unimplemented unwrap( {}, {} ) called", zero_point.raw_value_, checkpoint );
-  uint64_t max = (uint64_t)1 << 32;
+  uint64_t max = 1ULL << 32;
+  uint64_t offset = this->raw_value_ - zero_point.raw_value_;
   uint64_t n = checkpoint / max;
 
-  if(n == 0){
-    return checkpoint + 1;
-  }
-
-  uint64_t lower = (n - 1) * max + zero_point.raw_value_;
-  uint64_t mid = n * max + zero_point.raw_value_;
-  uint64_t upper = (n + 1) * max + zero_point.raw_value_;
+  uint64_t mid = n * max + offset;
+  uint64_t lower = (n > 0) ? (n - 1) * max + offset : mid;
+  uint64_t upper = (n + 1) * max + offset;
 
   uint64_t l = (checkpoint > lower) ? checkpoint - lower: (lower - checkpoint);
   uint64_t m = (checkpoint > mid) ? checkpoint - mid: (mid - checkpoint);
   uint64_t u = (checkpoint > upper) ? checkpoint - upper: (upper - checkpoint);
 
-  uint64_t cand = std::min(l, std::min(m, u));
-
-  return cand * max + zero_point.raw_value_;
+  return (l <= m && l <= u) ? lower : 
+            (m <= u) ? mid 
+            : upper;
 
 
   // return {};
